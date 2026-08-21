10af8be16f8e7e74431813e3176dd442d0d13c29
kinggot1004 <kinggot1004@gmail.com>
2026-08-21 11:49:00 +0900

    abcd

---
+++ 01_datatype/ex1.py
@@ -0,0 +1,17 @@
+#변수
+a = 2
+b = 3
+print(a,b)
+a,b = 2,3
+print(a,b)
+a,b = b,a
+print(a,b)
+#변수명 규칙(C와 동일)
+# 알파벳 숫자 특수문자 _ 만 가능
+# 숫자로 시작 불가
+# 예약어 금지
+이름 = "뽀로로"
+print(이름)
+studenr_name = "크롱" # snake
+StudenrName = "크롱" # camel
+MAX_SCORE = 100;
---
+++ 01_datatype/ex2.py
@@ -0,0 +1,39 @@
+# 파이썬 자료형
+# 1. 기본 자료형: 숫자형(정수형, 실수형), 불리언, 문자열
+# 2. 컬렉션 자료형: 리스트, 튜플, 딕셔너리, 집합
+
+# 숫자형 - 정수형(int)
+a = 10
+print(a,type(a))
+
+print(bin(a),oct(a),hex(a))
+print(ord("A"),chr(65))
+# x = 10**100
+# print(x)
+# a = 2**31 + 2
+# print(a)
+# 실수형(float)
+b = 3.14
+print(b,type(b))
+#float의 표현 범위
+# 부동 소수점 방식
+# 64비트 = 부호비트(1) + 지수부(11) + 가수부(52)
+import sys
+print(sys.float_info.min)
+print(sys.float_info.max)
+print(-sys.float_info.min)
+print(-sys.float_info.max)
+a = 1.7e308
+b = 1.8e308
+print(a,b)
+# 실수의 오차
+print(0.1 + 0+2 == 0.3)
+print(f"{0.1:.20f}")
+print(f"{0.2:.20f}")
+print(0.1)
+
+# 형변환
+print(float(10))
+print(int(3.14))
+print(float("100"))
+print(float("3.14"))
